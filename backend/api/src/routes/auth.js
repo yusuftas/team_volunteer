bf31fcdf3edaafa3399bfb163d18adf2efa62a9e
Yusuf Tas <ytas@department13.com>
2023-07-01 22:40:25 +1000

    Correctly handling configuration through env

--- backend/api/src/routes/auth.js
+++ backend/api/src/routes/auth.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const { JWT_SECRET } = require('../config/config');
+
 
 const router = express.Router();
 
@@ -12,11 +13,14 @@ router.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({ username, password: hashedPassword });
+
+    console.log(user);
     await user.save();
 
     res.status(201).json({ message: 'User registered successfully.' });
   } catch (error) {
     res.status(500).json({ message: 'Failed to register user.' });
+    console.log(error);
   }
 });
 
@@ -25,6 +29,7 @@ router.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
 
+    console.log(user);
     if (!user) {
       return res.status(404).json({ message: 'User not found.' });
     }
@@ -34,7 +39,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ message: 'Invalid username or password.' });
     }
 
-    const token = jwt.sign({ username, role: user.role }, JWT_SECRET);
+    const token = jwt.sign({ username, role: user.role }, process.env.JWT_SECRET);
     res.json({ token });
   } catch (error) {
     res.status(500).json({ message: 'Failed to login.' });
--- backend/api/src/server.js
+++ backend/api/src/server.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const express = require('express');
+
 const mongoose = require('mongoose');
-const { PORT, MONGODB_URI } = require('./config/config');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 
 const app = express();
+
+console.log(process.env.PORT);
+console.log(process.env.MONGODB_URI);
 
 // Middleware
 app.use(express.json());
@@ -14,12 +18,12 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
 // Connect to MongoDB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB.');
     // Start the server
-    app.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}.`);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server listening on port ${process.env.PORT}.`);
     });
   })
   .catch((error) => {
